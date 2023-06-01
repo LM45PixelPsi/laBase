c1d3161c6e2ab729ca973a150ea00be8de954820
Votre nom d'utilisateur <128391312+LM45PixelPsi@users.noreply.github.com>
2023-06-01 21:20:30 +0200

    1ere version terminée

--- assets/js/svg.js
+++ assets/js/svg.js
@@ -1,14 +1,18 @@
-const inclSVG = (svgContainerId) => {
+const inclSVG = async (svgContainerId) => {
   const svgPath = `./assets/visual/svg/${svgContainerId}.svg`;
 
-  fetch(svgPath)
-    .then((response) => {
-      return response.text();
-    })
-    .then((svgText) => {
-      const svgContainer = document.getElementById(svgContainerId);
-      svgContainer.innerHTML = svgText;
-    });
+  try {
+    const response = await fetch(svgPath);
+    const svgText = await response.text();
+
+    const svgContainer = document.getElementById(svgContainerId);
+    svgContainer.innerHTML = svgText;
+  } catch (error) {
+    console.error(`Error fetching SVG: ${error}`);
+  }
 };
 
-inclSVG("svgtest4");
+// inclSVG("");
+
+// mettre dans la funct le nom du svg placer dans le dossier (s'assurer que la div à l'id correspondant au nom du svg
+// ex :      <div class="svg-container" id="svgtest4"></div> )
