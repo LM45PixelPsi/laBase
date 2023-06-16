4ee73f4df1d0a8d9d57624cb967235837e00e204
Votre nom d'utilisateur <128391312+LM45PixelPsi@users.noreply.github.com>
2023-06-16 15:02:58 +0200

    modif de svg.js : data-svgi au lieu de id

--- assets/js/svg.js
+++ assets/js/svg.js
@@ -1,21 +1,29 @@
-const inclSVG = async (...svgContainerIds) => {
-  const fetchPromises = svgContainerIds.map((svgContainerId) => {
-    const svgPath = `./assets/svg/${svgContainerId}.svg`;
-    return fetch(svgPath)
-      .then((response) => response.text())
-      .then((svgText) => {
-        const svgContainer = document.getElementById(svgContainerId);
-        svgContainer.innerHTML = svgText;
-      })
-      .catch((error) => {
-        console.error(`Error fetching SVG for ${svgContainerId}: ${error}`);
-      });
-  });
+const inclSVG = async (...svgContainerDataAttributes) => {
+  const fetchPromises = svgContainerDataAttributes.map(
+    (svgContainerDataAttribute) => {
+      const svgPath = `./assets/visual/svg/${svgContainerDataAttribute}.svg`;
+      return fetch(svgPath)
+        .then((response) => response.text())
+        .then((svgText) => {
+          const svgContainers = document.querySelectorAll(
+            `[data-svgi="${svgContainerDataAttribute}"]`
+          );
+          svgContainers.forEach((svgContainer) => {
+            svgContainer.innerHTML = svgText;
+          });
+        })
+        .catch((error) => {
+          console.error(
+            `Error fetching SVG for ${svgContainerDataAttribute}: ${error}`
+          );
+        });
+    }
+  );
 
   await Promise.all(fetchPromises);
 };
 
 // inclSVG("", "");
 
-// mettre dans la funct le nom du svg placer dans le dossier (s'assurer que la div à l'id correspondant au nom du svg
-// ex :      <div class="svg-container" id="svgtest4"></div> )
+// mettre dans la funct le nom du svg placer dans le dossier (s'assurer que la div à la data-svgi correspondant au nom du svg
+// ex :      <div class="svg-container" data-svgi="leNomDuSvg"></div> )
